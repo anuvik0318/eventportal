0815e39fe66c6e1be3ff860e62bd82996f413fb5
vkovuri <vkovuri@BT-DEV-19-1.BTGI.local>
2019-07-29 17:48:02 -0400

    Cross origin

--- eventportal-service/src/main/java/com/eventportal/service/TestController.java
+++ eventportal-service/src/main/java/com/eventportal/service/TestController.java
@@ -1,12 +1,19 @@
 package com.eventportal.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController		
+@CrossOrigin("http://localhost:4200")
 public class TestController {
+	
 	@GetMapping("/test")
-	public String get() {
-		return "great";
+	@ResponseBody
+	public ResponseEntity<String> get() {
+		return new ResponseEntity<String>("Anvik Event Portal", HttpStatus.OK);
 	}
 }
