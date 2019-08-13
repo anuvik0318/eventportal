9a67a3b021b36181218948424efefd84090c2671
vkovuri <vkovuri@BT-DEV-19-1.BTGI.local>
2019-08-13 17:19:31 -0400

    Updated client profile page

--- eventportal-service/src/main/webapp/eventportal-ui/src/app/clients/clients.module.ts
+++ eventportal-service/src/main/webapp/eventportal-ui/src/app/clients/clients.module.ts
@@ -3,7 +3,7 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { MatIconModule, MatCardModule, MatButtonModule, MatListModule, MatProgressBarModule, MatMenuModule } from '@angular/material';
+import { MatIconModule, MatCardModule, MatButtonModule, MatListModule, MatProgressBarModule, MatMenuModule, MatGridListModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { ClientProfileComponent } from './client-profile/client-profile.component';
@@ -12,18 +12,8 @@ import { ClientListComponent } from './client-list/clientlist.component';
 import { ClientsRoutes } from './clients.routing';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild(ClientsRoutes),
-     MatIconModule,
-
-   FlexLayoutModule
-  ],
-  declarations: [ 
-    ClientProfileComponent,
-    ClientListComponent
-  ]
+  imports: [CommonModule, FormsModule, RouterModule.forChild(ClientsRoutes), MatIconModule, MatCardModule, MatButtonModule, MatListModule, MatProgressBarModule, 
+    MatMenuModule, FlexLayoutModule, MatGridListModule],
+  declarations: [ClientProfileComponent, ClientListComponent]
 })
-
 export class ClientsModule {}
